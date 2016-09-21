8c342f38ea51c6e979109b97c57c53fcb6d4b746
Jonas <lieber.jonas@outlook.de>
2016-09-21 20:19:18 +0200

    While loop, hasWon function

--- main.py
+++ main.py
@@ -3,22 +3,42 @@ import math
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome('chromedriver/chromedriver.exe')
+driver = webdriver.Chrome('chromedriver/linux/chromedriver')
 driver.get('https://www.google.de/xhtml')
 search_box = driver.find_element_by_name('q')
 #send tic-tac-toe to the Gooogle Search Bar
 search_box.send_keys('tic-tac-toe')
 search_box.submit()
 time.sleep(2)
-googleboard = []
+
 
 
 board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 seed = 1
 oppSeed = -1
 
+def winningPatterns():
+    wp = ['111000000', '000111000', '000000111', '100100100', '010010010', '001001001', '100010001', '001010100']
+    r = range(len(wp))
+    for index, element in enumerate(wp):
+        r[index] = int(wp[index], 2)
+    return r
+
+def hasWon(player):
+    p = 0
+    for index, element in enumerate(board):
+        if(board[index] == player):
+            p |=  (1 << index)
+    for index, element in enumerate(winningPatterns()):
+        wp = winningPatterns()[index]
+        if (p & wp) == wp:
+            return True
+    return False
+
 def getValidMoves():
     nm = []
+    if (hasWon(seed)) or (hasWon(oppSeed)):
+        return nm
 
     for index, element in enumerate(board):
         if element == 0:
@@ -108,29 +128,39 @@ def minimax(depth, player):
 
     return [best, bestidx]
 
+while 1:
+    board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    googleboard = []
+    table = driver.find_element_by_class_name('_i7i')
+    rows = table.find_elements_by_tag_name('tr')
+    if len(rows) == 3:
+        for index, element in enumerate(rows):
+            columns = element.find_elements_by_tag_name('td')
+            if len(columns) == 3:
+                for i, elm in enumerate(columns):
+                    googleboard.append(elm)
+            else:
+                print('Tic-Tac-Toe Feld konnte nicht gefunden werden.')
+    else:
+        print('Tic-Tac-Toe Feld konnte nicht gefunden werden.')
 
-table = driver.find_element_by_class_name('_i7i')
-rows = table.find_elements_by_tag_name('tr')
-if len(rows) == 3:
-    for index, element in enumerate(rows):
-        columns = element.find_elements_by_tag_name('td')
-        if len(columns) == 3:
-            for i, elm in enumerate(columns):
-                googleboard.append(elm)
-        else:
-            print('Tic-Tac-Toe Feld konnte nicht gefunden werden.')
-else:
-    print('Tic-Tac-Toe Feld konnte nicht gefunden werden.')
 
-for num in range(0, 8):
-    move = minimax(2, seed)[1]
-    board[move] = seed
-    googleboard[move].click()
-    # wait for Google
+    for num in range(0, 8):
+        move = minimax(2, seed)[1]
+        board[move] = seed
+        googleboard[move].click()
+        if (hasWon(seed)):
+            break
+        # wait for Google
+        time.sleep(2)
+        for index, element in enumerate(googleboard):
+            if (board[index] != 1) and (board[index] != -1):
+                    svgs = element.find_elements_by_tag_name('svg')
+                    display = svgs[1].value_of_css_property('display')
+                    if display == 'block':
+                        board[index] = -1
+        if (hasWon(oppSeed)):
+            break
     time.sleep(5)
-    for index, element in enumerate(googleboard):
-        if (board[index] != 1) and (board[index] != -1):
-                svgs = element.find_elements_by_tag_name('svg')
-                display = svgs[1].value_of_css_property('display')
-                if display == 'block':
-                    board[index] = -1
+    driver.find_element_by_xpath('//*[@id="rso"]/div[1]/div[1]/div/div[1]/div[2]/div/div/div[2]/div').click()
+    time.sleep(2)
